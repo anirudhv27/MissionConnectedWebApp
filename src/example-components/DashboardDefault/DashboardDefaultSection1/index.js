c31fd14fa9a7bac06bdf9dfe03b29027d4846507
crystalrchen <crystalrchen@users.noreply.github.com>
2021-04-18 18:43:38 -0700

    adding popup
    still has bugs, need the 3-dot menu to close when popup opens

--- src/example-components/DashboardDefault/DashboardDefaultSection1/index.js
+++ src/example-components/DashboardDefault/DashboardDefaultSection1/index.js
@@ -41,102 +41,37 @@ export default function LivePreviewExample() {
     {
       name: 'MSJ UNICEF',
       link: require('../../../assets/images/clubs-bg/unicef.jpg'),
-      description: "UNICEF, also greatly known as the United Nations International Children's Emergency Fund, is a United Nations agency responsible for providing humanitarian and developmental aid to children worldwide.",
+      description: "UNICEF, also greatly known as the United Nations International Children's Emergency Fund, is a United Nations agency responsible for providing humanitarian and developmental aid to children worldwide. MSJ UNICEF hopes to contribute to this cause and holds monthly fundraisers and events to accomplish this.",
+      members: '32',
+      meetings: 'Monthly meetings @ lunch',
     },
     {
       name: 'MSJ Girls Who Code',
       link: require('../../../assets/images/clubs-bg/girlswhocode.jpg'),
       description: "MSJ Girls Who Code welcomes all levels of coders on campus looking to build a community and expand their coding skill set. Throughout the year, we host speakers, go on fieldtrips, attend hackathons as a club, and complete personal projects in multiple languages. In the past years, we’ve met Reshma Saujani, the CEO of GWC, visited Netflix and Google, and invited speakers from a vast range of fields. We compete at CodeDay annually and teach many languages including HTML, Javascript, Python, etc. Join for a new learning experience every week! Join our Facebook group (@MSJ Girls Who Code) for up to date meeting information and email us at msjgwc@gmail.com with any questions. ",
+      members: '70',
+      meetings: 'Weekly meetings on Wednesdays @ 3:00-4:30',
     },
     {
       name: 'MSJ Biotechnology Club',
       link: require('../../../assets/images/clubs-bg/biotechnology.jpg'),
       description: "MSJ Biotechnology Club is a student organization for students interested in careers in the STEM field. We explore the link between healthcare and technology through lab work and research experiment. We listen to guest speakers talk about careers in this field, discuss journal articles, and participate in local STEM focused events. Whether you're iterested in medicine, robotics, or research, MSJ Biotechnology Club will help enrich your understanding in this vast field.",
+      members: '19',
+      meetings: 'Weekly meetings on Fridays @ 2:30-4:00',
     },
     {
       name: 'MSJ Computer Science Club',
       link: require('../../../assets/images/clubs-bg/cs.jpg'),
       description: "We meet on Fridays from 3 to 4 pm on our Discord server (join using this link: https://discord.gg/JWu6KPz). Please also join our Facebook Group, MSJ Compsci Club. If you have any questions, ask on Discord, contact an officer on Messenger, or email msjhscompsci@gmail.com.",
+      members: '46',
+      meetings: 'Weekly meetings on Fridays @ 3:00-4:30',
     }
   ];
 
-  
-  // const chart30Options = {
-  //   chart: {
-  //     toolbar: {
-  //       show: false
-  //     },
-  //     sparkline: {
-  //       enabled: true
-  //     }
-  //   },
-  //   dataLabels: {
-  //     enabled: false
-  //   },
-  //   colors: ['#3c44b1'],
-  //   stroke: {
-  //     color: '#4191ff',
-  //     curve: 'smooth',
-  //     width: 4
-  //   },
-  //   xaxis: {
-  //     crosshairs: {
-  //       width: 1
-  //     }
-  //   },
-  //   yaxis: {
-  //     min: 0
-  //   },
-  //   legend: {
-  //     show: false
-  //   }
-  // };
-  // const chart30Data = [
-  //   {
-  //     name: 'Customers',
-  //     data: [47, 38, 56, 24, 45, 54, 38, 47, 38, 56, 24, 56, 24, 65]
-  //   }
-  // ];
 
-  // const chart31Options = {
-  //   chart: {
-  //     toolbar: {
-  //       show: false
-  //     },
-  //     sparkline: {
-  //       enabled: true
-  //     }
-  //   },
-  //   dataLabels: {
-  //     enabled: false
-  //   },
-  //   colors: ['#f4772e'],
-  //   stroke: {
-  //     color: '#4191ff',
-  //     curve: 'smooth',
-  //     width: 3
-  //   },
-  //   xaxis: {
-  //     crosshairs: {
-  //       width: 1
-  //     }
-  //   },
-  //   yaxis: {
-  //     min: 0
-  //   },
-  //   legend: {
-  //     show: false
-  //   }
-  // };
-  // const chart31Data = [
-  //   {
-  //     name: 'Sales',
-  //     data: [47, 38, 56, 24, 45, 54, 38, 47, 38, 56, 24, 56, 24, 65]
-  //   }
-  // ];
   function generateClubs(clubs) {
     return clubs.map((club) => {
-      return <Club name={club.name} link={club.link} description={club.description}/>;
+      return <Club name={club.name} link={club.link} description={club.description} members={club.members} meetings={club.meetings}/>;
     });
   }
 
