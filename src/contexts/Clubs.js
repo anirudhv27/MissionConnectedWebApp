5f90835e18272de680e0812deed0289e1deff2a5
nealgithub10 <68788552+nealgithub10@users.noreply.github.com>
2021-09-25 14:53:21 -0700

    Joinclub (#13)
    * Adding join club button
    
    * Button onclick handler
    
    * Updating the firebase database with clubs user joined
    
    Co-authored-by: Vignesh Rangarajan <vignesh@vigneshs-air.lan>

--- src/contexts/Clubs.js
+++ src/contexts/Clubs.js
@@ -6,34 +6,57 @@ import ExpansionPanelDetails from '@material-ui/core/ExpansionPanelDetails';
 import ExpansionPanelSummary from '@material-ui/core/ExpansionPanelSummary';
 import ExpandMoreIcon from '@material-ui/icons/ExpandMore';
 import { Typography } from '@material-ui/core';
+import { Button } from '@material-ui/core';
+import { useAuth } from "./AuthContext";
 
   
 export function DBClubsList({ children }) {
   const [clubs, setClubs] = useState();
   const [loading, setLoading] = useState(true);  
   const [expanded, setExpanded] = React.useState(false);
+  const { currentUser } = useAuth();
 
   const handleChange = panel => (event, isExpanded) => {
     setExpanded(isExpanded ? panel : false);
   };
 
+  const handleJoinClub = clubId =>  {
+    const usersClubsRef = database.ref().child('users_clubs')
+    usersClubsRef.child(currentUser.uid+clubId).set({
+      role: "Member",
+      userId: currentUser.uid,
+      clubId: clubId
+    }, (error) => {
+      if (error) {
+        alert("Could Not add user to club");
+      } else {
+        alert("Add user to club");
+      }
+    });
+  }
+
+
   function generateClubs() {
     if(clubs!=undefined) {
       var keys = Array.from( clubs.keys() );
-      console.log("keys " + keys.length);
+      console.log("keys " + keys);
       return keys.map(key => (
         <ExpansionPanel
+        key={key}
         expanded={expanded === key}
         onChange={handleChange(key)}>
         <ExpansionPanelSummary
           expandIcon={<ExpandMoreIcon />}
-          aria-controls="{key}-content"
-          id="{key}-header">
+          aria-controls={key + "-content"}
+          id={key+"-header"}>
           <Typography>{clubs.get(key).get('club_name')}</Typography>
         </ExpansionPanelSummary>
         <ExpansionPanelDetails>
           <Typography>
           {clubs.get(key).get('club_description')}
+          <Button className="m-2" variant="contained" color="secondary" onClick = {() => handleJoinClub(key)}>
+            Join Club
+          </Button>
           </Typography>
         </ExpansionPanelDetails>
       </ExpansionPanel>
